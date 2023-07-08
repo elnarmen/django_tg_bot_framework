393c535b66a9484a893930dcd5b7fae88241b586
Evgeny Evseev <pelid80@gmail.com>
2023-07-08 14:46:34 +0300

    Remove redirect_tg_commands decorator

--- README.md
+++ README.md
@@ -108,7 +108,6 @@ rootdir: /opt/app/src
 plugins: django-4.5.2, httpx-0.22.0, anyio-3.7.0
 collected 23 items
 
-test_decorators.py ......                             [ 26%]
 test_route_validation.py ........                     [ 60%]
 test_router.py ...                                    [ 73%]
 test_state_machine.py .....                           [ 95%]
@@ -117,11 +116,21 @@ test_states.py .                                      [100%]
 ================= 6 passed in 0.22s ========================
 ```
 
-Если вы чините поломанный тест, часто его запускаете и не хотите ждать когда отработают остальные, то можно запускать их по-отдельности. При этом полезно включать опцию `-s`, чтобы pytest не перехватывал вывод в консоль и выводил все сообщения. Пример для теста `test_redirect_tg_commands` из файла `tests/test_decorators.py.py`:
+Если вы чините поломанный тест, часто его запускаете и не хотите ждать когда отработают остальные, то можно запускать их по-отдельности. При этом полезно включать опцию `-s`, чтобы pytest не перехватывал вывод в консоль и выводил все сообщения. Пример для теста `test_state_ignore_unknown_args` из файла `tests/test_states.py`:
 
 ```shell
-$ docker compose run --rm django_tg_bot_framework pytest -s tests/test_decorators.py::test_redirect_tg_commands
+$ docker compose run --rm django_tg_bot_framework pytest -s tests/test_states.py::test_state_ignore_unknown_args
+================ test session starts =======================
+platform linux -- Python 3.11.4, pytest-7.3.2, pluggy-1.2.0
+django: settings: tests.settings (from ini)
+rootdir: /opt/app
+configfile: pyproject.toml
+plugins: anyio-3.7.0, django-4.5.2, httpx-0.22.0
+collected 1 item
 
+tests/test_states.py .
+
+================= 1 passed in 0.21s ========================
 ```
 
 Подробнее про [Pytest usage](https://docs.pytest.org/en/6.2.x/usage.html).
--- tests/test_decorators.py
+++ tests/test_decorators.py
@@ -1,26 +1,21 @@
-import pytest
+from typing import Any, Type
 
-from tg_api import Update
-
-from django_tg_bot_framework.decorators import redirect_tg_commands
-from django_tg_bot_framework.routes import Router
+from django_tg_bot_framework.routes import Router, StateDecoratorType
 from django_tg_bot_framework.states import BaseState
 from django_tg_bot_framework.state_machine import StateMachine
 
 
-@pytest.mark.parametrize(
-    "text,locator",
-    [
-        (None, '/'),
-        ('some text', '/'),
-        ('/starting', '/'),
-        ('/start', '/start/'),
-        ('/stArt', '/start/'),
-        ('/start with extra text', '/start/'),
-    ],
-)
-def test_redirect_tg_commands(text: str | None, locator: str):
-    router = Router(decorators=[redirect_tg_commands])
+def test_simple_logging_decorator():
+    process_calls_log = []
+
+    def log_process_calls(state_class: Type[BaseState]) -> StateDecoratorType:
+        class WrappedStateClass(state_class):
+            def process(self, event: Any) -> BaseState | None:
+                process_calls_log.append(event)
+                return super().process(event=event)
+        return WrappedStateClass
+
+    router = Router(decorators=[log_process_calls])
 
     @router.register('/', title='Корневое состояние бота')
     class RootState(BaseState):
@@ -30,22 +25,13 @@ def test_redirect_tg_commands(text: str | None, locator: str):
     class StartState(BaseState):
         pass
 
-    update = Update(
-        update_id=1,
-        message={
-            'message_id': 1,
-            'date': 1,
-            'chat': {
-                'id': 43,
-                'type': 'private',
-            },
-        },
-    )
-    if text is not None:
-        update.message.text = text
-
     state_machine = StateMachine(current_state=router.locate('/'))
     state_machine.reenter_state()
-    state_machine.process(update)
+    state_machine.process('first_event')
+    state_machine.switch_to(router.locate('/start/'))
+    state_machine.process('second_event')
 
-    assert state_machine.current_state.state_class_locator == locator
+    assert process_calls_log == [
+        'first_event',
+        'second_event',
+    ]
